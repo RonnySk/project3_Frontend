ac84bf8678c9e1386a9f8021962b69f64500f23b
RonnySk <ronny_sk@hotmail.com>
2023-06-28 13:03:54 +0200

    add first route to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/LoginPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div>
+      <p>Login test Page</p>
+    </div>
+  );
+}
+
+export default Login;
