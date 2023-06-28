a51342f1bda86e8414cef50a34a070b59ce947a1
RonnySk <ronny_sk@hotmail.com>
2023-06-28 13:11:01 +0200

    create HomePage, add to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./components/HomePage";
 import Login from "./components/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
