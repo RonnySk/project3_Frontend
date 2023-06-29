3466745d2fe0379edfa6e8e4f6f3c0d7f066b9f7
RonnySk <ronny_sk@hotmail.com>
2023-06-29 14:52:31 +0200

    update signupPage

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,7 +1,54 @@
-function SignupPage() {
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
+
+function SignupPage(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rsAgent, setrsAgent] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handlersAgent = (e) => setrsAgent(e.target.value);
+
+  const handleSignupSubmit = (e) => {};
+
   return (
-    <div>
-      <p>SignupPage</p>
+    <div className="SignupPage">
+      <h1>Sign Up</h1>
+
+      <form onSubmit={handleSignupSubmit}>
+        <label>Email:</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <label>Are you Real Estate Agent?:</label>
+        <input
+          type="checkbox"
+          name="rsAgent"
+          value={rsAgent}
+          onChange={handlersAgent}
+        />
+
+        <button type="submit">Sign Up</button>
+      </form>
+
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Already have account?</p>
+      <Link to={"/loginPage"}> Login</Link>
     </div>
   );
 }
