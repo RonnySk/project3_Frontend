106bb1ce10986b6987c3634efc7ed58635b7aa3f
RonnySk <ronny_sk@hotmail.com>
2023-07-04 21:54:50 +0200

    .env create and Calculator working

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,17 +6,45 @@ function Calculator() {
   const [interestRate, setInterestRate] = useState();
   const [duration, setDuration] = useState();
 
+  const handleloanAmount = (e) => setLoanAmount(e.target.value);
+  const handleinterestRate = (e) => setInterestRate(e.target.value);
+  const handleduration = (e) => setDuration(e.target.value);
+
+  const handleCalculatorSubmit = (e) => {
+    e.preventDefault();
+    // const requestBody = { loanAmount, interestRate, duration };
+
+    const config = {
+      headers: {
+        // "X-Api-Key": `${process.env.X_API_KEY}`,
+        "X-Api-Key": "Kp49p/WlYVEDj7PvUuMfzA==oLGVdjm3HJQql4SN",
+      },
+    };
+
+    const url = `https://api.api-ninjas.com/v1/mortgagecalculator?loan_amount=${loanAmount}&interest_rate=${interestRate}&duration_years=${duration}`;
+
+    axios
+      .get(url, config)
+      .then((response) => {
+        console.log("Response from calculator API", response.data);
+      })
+      .catch((error) => {
+        // const errorDescription = error.response.data.message;
+        // setErrorMessage(errorDescription);
+      });
+  };
+
   return (
     <div>
       <h1>Mortgage Calculator</h1>
 
-      <form>
+      <form onSubmit={handleCalculatorSubmit}>
         <label>Loan Amount</label>
         <input
           type="number"
           name="loanAmount"
           value={loanAmount}
-          //   onChange={handleloanAmount}
+          onChange={handleloanAmount}
         ></input>
 
         <label>Interest Rate</label>
@@ -24,7 +52,7 @@ function Calculator() {
           type="number"
           name="interestRate"
           value={interestRate}
-          //   onChange={handleinterestRate}
+          onChange={handleinterestRate}
         ></input>
 
         <label>Years</label>
@@ -32,8 +60,10 @@ function Calculator() {
           type="number"
           name="duration"
           value={duration}
-          //   onChange={handleduration}
+          onChange={handleduration}
         ></input>
+
+        <button type="submit">Calculate</button>
       </form>
     </div>
   );
