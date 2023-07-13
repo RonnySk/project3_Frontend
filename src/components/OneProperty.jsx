fa4b66e85cc0fcef4b2a24cae090bbdbd0380893
RonnySk <ronny_sk@hotmail.com>
2023-07-13 21:46:32 +0200

    axios.get oneProperty

--- src/components/OneProperty.jsx
+++ src/components/OneProperty.jsx
@@ -1,5 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+const API_URL = "http://localhost:5005";
+
 function OneProperty() {
-  return <div>teste</div>;
+  const [project, setProperty] = useState([]);
+  const { propertyId } = useParams();
+
+  const getProperty = () => {
+    axios
+      .get(`${API_URL}/property/${propertyId}`)
+      .then((response) => {
+        const oneProperty = response.data;
+        setProperty(oneProperty);
+        console.log(" one propertie", oneProperty);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getProperty();
+  }, []);
+
+  return (
+    <div>
+      <p>{project.title}</p>
+    </div>
+  );
 }
 
 export default OneProperty;
